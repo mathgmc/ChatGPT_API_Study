ed12a414b53a2f6b40c6d97297c85fc0dd1a2b59
Matheus Carvalho <matheusgmc@hotmail.com>
2024-11-04 09:15:35 -0300

    Add count tokens script and documantation

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ Repository dedicated to study how to use ChatGPT API. This repository is made of
 
 ### list_countries.py
 
-Script that list contries from a selected continent.
+Script that list countries from a selected continent.
 
 ### classifier.py
 
 Script that classifies some product in one of the predefined categories and describe the category, generating 3 responses. On this code we play with a dynamic prompt and some OpenAI parameters like temperature, max_tokens and n. 
 
+### count_tokens.py
+
+Script that uses tiktokens lib to count tokens used and calculate how much this token costs on two different models.
---
+++ count_tokens.py
@@ -0,0 +1,19 @@
+import tiktoken
+
+model = "gpt-4"
+codificador = tiktoken.encoding_for_model(model)
+tokens_list = codificador.encode("You are a product categorizer.")
+
+print("Tokens List: ", tokens_list)
+print("Total tokens: ", len(tokens_list))
+print(f"Cost for model {model} is ${(len(tokens_list)/1000) * 0.03}")
+
+model = "gpt-3.5-turbo-1106"
+codificador = tiktoken.encoding_for_model(model)
+tokens_list = codificador.encode("You are a product categorizer.")
+
+print("Tokens List: ", tokens_list)
+print("Total tokens: ", len(tokens_list))
+print(f"Cost for {model} is ${(len(tokens_list)/1000) * 0.001}")
+
+print(f"The cost of GPT4 is {0.03/0.001} times higher than GPT 3.5-turbo")
