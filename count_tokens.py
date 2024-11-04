6929df87534604d9108c7a9f4538cb61bc5a80dc
Matheus Carvalho <matheusgmc@hotmail.com>
2024-11-04 09:39:44 -0300

    Fixing var name

--- count_tokens.py
+++ count_tokens.py
@@ -1,16 +1,16 @@
 import tiktoken
 
 model = "gpt-4"
-codificador = tiktoken.encoding_for_model(model)
-tokens_list = codificador.encode("You are a product categorizer.")
+codifier = tiktoken.encoding_for_model(model)
+tokens_list = codifier.encode("You are a product categorizer.")
 
 print("Tokens List: ", tokens_list)
 print("Total tokens: ", len(tokens_list))
 print(f"Cost for model {model} is ${(len(tokens_list)/1000) * 0.03}")
 
 model = "gpt-3.5-turbo-1106"
-codificador = tiktoken.encoding_for_model(model)
-tokens_list = codificador.encode("You are a product categorizer.")
+codifier = tiktoken.encoding_for_model(model)
+tokens_list = codifier.encode("You are a product categorizer.")
 
 print("Tokens List: ", tokens_list)
 print("Total tokens: ", len(tokens_list))
